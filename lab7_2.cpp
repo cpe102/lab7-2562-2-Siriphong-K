a48183431e9d6795582c69df6b1c4716b525a586
lab314-54@labeng.local <lab314-54@labeng.local>
2020-01-21 16:30:40 +0700

    Terst

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -2,24 +2,34 @@
 #include<iomanip>
 #include<string>
 using namespace std;
-
 //[Missing Code 1] Write definition of the function findGrade() here.
-
+char findGrade(double a){
+   return (a>90?'A':a<90&&a>75?'B':a<75&&a>60?'C':a<60&&a>45?'D':'F');
+	
+	
+	
+	
+	//if(a>90.00){return 'A'}
+	//else if(a>75.00&&a<90){return 'B'}
+	//else if(a>60.00&&a<75){return 'C'}
+	//else if(a>45.00&&a<60){return 'D'}
+	//else if(a<=45.00){return 'F'}
+}
 int main(){
 	//Input the number of students
 	int N,i = 0;
 	cout << "Enter the number of students: ";
 	cin >> N;
 	string name[N];
-	float score[N];	
+	double score[N];	
 	
 	//Store names and scores of students into an array 
 	while(i < N){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
-		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+		getline(cin,name[i]);
 		cout << "Score of student " << i+1 << ": ";
-		//[Missing Code 3] Get score of the i-th students.
+		cin >> score[i];
 		i++;
 	}
 	
--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,1 +1,34 @@
-//status = "ONE-NIGHT-STAND";
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string status;
+	int age,hgt,money;
+cout<<"Input Age :";
+cin>>age;
+if(age<=20){
+cout<<"Input Height :";
+cin.ignore();
+cin>>hgt;
+if(hgt<160){status="UNFRIEND";}
+else if(hgt<175){status="FRIEND";}
+else{
+cout<<"Input Money(Million) :";
+cin.ignore();
+cin>>money;
+if(money>69){status="MARRIED";}
+else{status="ONE-NIGHT-STAND";}
+}
+}
+else if(age<30){
+cout<<"Input Money(Million) :";
+cin.ignore();
+cin>>money;
+if(money>10){status="BEST FRIEND";}
+else{status="UNFRIEND";} 
+}
+else{status="UNFRIEND";}
+cout<<status;
+}
